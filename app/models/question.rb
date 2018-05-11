63e917547150abccdb136b2199abffb424caf1a1
andreydro <andreydro14@gmail.com>
2018-05-11 17:14:25 +0300

    Work-with-responders

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers
   has_many :attachments, as: :attachmentable
+  has_many :comments, as: :commentable
 
   validates :title, :body, presence: true
 
