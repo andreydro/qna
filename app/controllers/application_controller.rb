7eaec78c9e87414581c91fd4e218966f7069fd0d
andreydro <andreydro14@gmail.com>
2018-05-15 12:15:05 +0300

    Work-with-cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
+  #check_authorization
 end
